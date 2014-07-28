8a02acdb37a93c6a76dbee27b84d233776e51472
Alex Thomassen <decicus@gmail.com>
2014-07-28 22:48:17 +0200

    Added "pmute" and "pmuteid".
    Updated addon.txt

---
+++ lua/ulx/modules/sh/decicus_chat.lua
@@ -0,0 +1,106 @@
+local CATEGORY_NAME = "Chat"
+
+-- Permmute sort of inspired by Cobalt's "pgag".
+function ulx.pmute( calling_ply, target_ply, should_unpmute )
+
+	if should_unpmute then
+	
+		target_ply:RemovePData( "ULX_PermMute" )
+		ulx.fancyLogAdmin( calling_ply, "#A unpermmuted #T.", target_ply )
+		
+	else
+	
+		target_ply:SetPData( "ULX_PermMute", "true" )
+		ulx.fancyLogAdmin( calling_ply, "#A permmuted #T.", target_ply )
+		
+	end
+
+end
+local pmute = ulx.command( CATEGORY_NAME, "ulx pmute", ulx.pmute, "!pmute" )
+pmute:addParam{ type=ULib.cmds.PlayerArg }
+pmute:addParam{ type=ULib.cmds.BoolArg, invisible=true }
+pmute:defaultAccess( ULib.ACCESS_ADMIN )
+pmute:help( "Mutes the target permanently." )
+pmute:setOpposite( "ulx unpmute", { _, _, true }, "!unpmute" )
+
+function ulx.pmuteid( calling_ply, sid, should_unpmuteid )
+
+	local sid = string.upper( sid )
+
+	if not ULib.isValidSteamID( sid ) then
+	
+		ULib.tsayError( calling_ply, "Invalid Steam ID!" )
+		
+	else
+	
+		if should_unpmuteid then
+		
+			util.RemovePData( sid, "ULX_PermMute" )
+			ulx.fancyLogAdmin( calling_ply, "#A unpermmuted the Steam ID: #s", sid )
+		
+		else
+		
+			util.SetPData( sid, "ULX_PermMute", "true" )
+			ulx.fancyLogAdmin( calling_ply, "#A permmuted the Steam ID: #s", sid )
+		
+		end
+	
+	end
+
+end
+local pmuteid = ulx.command( CATEGORY_NAME, "ulx pmuteid", ulx.pmuteid, "!pmuteid" )
+pmuteid:addParam{ type=ULib.cmds.StringArg, hint="Player Steam ID." }
+pmuteid:addParam{ type=ULib.cmds.BoolArg, invisible=true }
+pmuteid:defaultAccess( ULib.ACCESS_ADMIN )
+pmuteid:help( "Mutes a player's Steam ID permanently." )
+pmuteid:setOpposite( "ulx unpmuteid", { _, _, true }, "!unpmuteid" )
+
+function CheckPMuted( ply, str, t )
+
+	if ply:GetPData( "ULX_PermMute" ) == "true" then
+	
+		ULib.tsayError( ply, "You are permanently muted and cannot speak." )
+		return ""
+	
+	end
+
+end
+hook.Add( "PlayerSay", "Check_Perm_Muted", CheckPMuted )
+
+function NotifyPMuteJoin( ply )
+
+	if ply:GetPData( "ULX_PermMute" ) == "true" then
+	
+		for _, mods in ipairs( player.GetAll() ) do
+		
+			if ucl.query( mods, "ulx pmute" ) then
+			
+				ULib.tsayError( mods, ply:Nick() .. " has joined the server and is permmuted." )
+			
+			end
+		
+		end
+	
+	end
+
+end
+hook.Add( "PlayerInitialSpawn", "NotifyPMuteJoin", NotifyPMuteJoin )
+
+function NotifyPMuteLeave( ply )
+
+	if ply:GetPData( "ULX_PermMute" ) == "true" then
+	
+		for _, mods in ipairs( player.GetAll() ) do
+		
+			if ucl.query( mods, "ulx pmute" ) then
+			
+				ULib.tsayError( mods, ply:Nick() .. " has left the server and is permmuted." )
+			
+			end
+		
+		end
+	
+	end
+
+end
+hook.Add( "PlayerDisconnected", "NotifyPMuteLeave", NotifyPMuteLeave )
